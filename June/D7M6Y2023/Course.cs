9af2522a25cce92fe822135fd6f581f7e1428f79
DuckQuack <64895072+ChheavBunHeng@users.noreply.github.com>
2023-06-02 23:58:11 +0700

    Add files via upload

---
+++ June/D7M6Y2023/Course.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace D7M6Y2023
+{
+    public class Course
+    {
+        public int ID {get;set;}
+        public string Name {get;set;}
+        public Course(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+        
+    }
+}
---
+++ June/D7M6Y2023/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Principal;
+using System;
+using System.Collections.Generic;
+using D7M6Y2023;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        Student Jim = new Student(123,"Duck");
+        Jim.StudentList();
+        Teacher John = new Teacher(456,"smith",250);
+        John.ShowInfo();
+        DatabaseConnection DBconnection = new DatabaseConnection();
+        DBconnection.OpenConnection();
+        
+    }
+}
+

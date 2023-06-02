190bfc5ed0676c0520095c734a71f4e05b78ac02
DuckQuack <64895072+ChheavBunHeng@users.noreply.github.com>
2023-06-02 23:52:08 +0700

    Add files via upload

---
+++ June/D06M01Y2023/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mysql.Data.MySqlClient;
+
+
+namespace D06M01Y2023
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;user=root;database=accountnika;port=3306;password=1234";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Connection Successful");
+            connection.Close();
+            Console.ReadKey();
+            
+        }
+    }
+}
